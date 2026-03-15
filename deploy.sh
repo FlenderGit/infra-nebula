bd2ce8426e48176f13f47dfffc93814ecc2ab161
FlenderGit <tristan.deloeil@gmail.com>
2026-03-15 11:33:54 +0100

    FIX: Add new versions

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,7 @@ set -a
 source .env
 set +a
 
+docker node update --label-add nats=1 ubuntu1-deloeilt
+docker node update --label-add nats=2 ubuntu2-deloeilt
+
 docker stack deploy -c stack.yml nebula
