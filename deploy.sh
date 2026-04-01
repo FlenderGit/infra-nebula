02a36d76b77839c91e9b4f3872e905da79533b35
FlenderGit <tristan.deloeil@gmail.com>
2026-04-01 18:30:48 +0200

    init

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -a
 source .env
 set +a
 
-docker node update --label-add nats=1 ubuntu1-deloeilt
-docker node update --label-add nats=2 ubuntu2-deloeilt
+docker node update --label-add nats=1 nebula-deloeit-2
+docker node update --label-add nats=2 nebula-deloeit-1
 
 docker stack deploy -c stack.yml nebula
